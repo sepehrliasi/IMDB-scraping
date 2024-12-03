eb8e37135d980c4efae42b7376b4e65128c5cb1c
Sepehr Liasi <sepehrghaseminejad@yahoo.com>
2024-12-03 17:41:35 +0330

    Write the exploited data into a csv file

--- scraper.py
+++ scraper.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-# import re
-# import pandas as pd
+import pandas as pd
 
 # Downloading imdb top 250 movie's data
 url = 'https://www.imdb.com/chart/top/'
@@ -13,11 +12,14 @@ soup = BeautifulSoup(response.text, "html.parser")
 
 movies_list = []
 for movie in soup.findAll('div', attrs={'class': 'sc-300a8231-0 gTnHyA cli-children'}):
-    film = {'title': movie.findNext('h3').text.split(' ', 1)[1],
-            'rank': movie.findNext('h3').text.split('.', 1)[0],
+    film = {'rank': movie.findNext('h3').text.split('.', 1)[0],
+            'title': movie.findNext('h3').text.split(' ', 1)[1],
             'date': movie.findNext('span', attrs={'class': 'sc-300a8231-7 eaXxft cli-title-metadata-item'}).text,
             'duration': movie.find('div', class_='sc-300a8231-6 dBUjvq cli-title-metadata').find_all('span')[1].text,
             'rating': movie.findNext('span', attrs={'class': 'ipc-rating-star--rating'}).text}
     movies_list.append(film)
 
-print(movies_list)
+# print(movies_list)
+
+df = pd.DataFrame(movies_list)
+df.to_csv('imdb_top_250_movies.csv', index=False)
